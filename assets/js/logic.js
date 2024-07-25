dda3d51d9de4ed9ac1ce841670c17b63c0bcb871
David <dumana92@gmail.com>
2024-07-24 21:00:45 -0400

    Build initial Logic js

---
+++ assets/js/logic.js
@@ -0,0 +1,23 @@
+const userList = document.querySelector('#list-group');
+
+// Remove List Item - Landing Page
+userList.addEventListener('click', function (event) {
+const target = event.target;
+if (target.matches('button') === true) {
+    const index = target.parentElement.getAttribute('data-index');
+    console.log(`Button index ${index} has been clicked`);
+  }
+});
+
+// Render List Item - Landing Page
+
+// Store List Item - Landing/Blog Page
+
+// Redirect to Blog Page - Landing Page
+let redirectURL = "";
+
+const redirectPage = function (url) {
+  redirectURL = url;
+  location.assign(url);
+};
+
